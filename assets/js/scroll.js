ef363b54acbd8182fb81295af70a610785358ac8
kunilovsky <kunilovsky@gmail.com>
2022-09-26 14:55:04 +0500

    fix BG-Scroll

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,6 +1,6 @@
 let scrollpos = window.scrollY
 const header = document.getElementById("scroll")
-const scrollChange = 200
+const scrollChange = 500
 const add_class_on_scroll = () => header.classList.add("navbar-bg")
 window.addEventListener('scroll', function() { 
   scrollpos = window.scrollY;
